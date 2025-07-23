c7bd2450ddb30f934d52a77a2f7a89da3f8c40b6
giovanaassis <giovana_2011_11@hotmail.com>
2025-07-23 18:36:26 -0300

    feat: added the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,18 @@
 import Brasil from "@/assets/brasil.png";
 import UK from "@/assets/united-kingdom.png";
 import { MoonIcon } from "lucide-react";
+import { useState } from "react";
 
 function Navbar() {
+  const [activeItem, setActiveItem] = useState<number>(0);
+  const navItems: string[] = [
+    "home",
+    "sobre mim",
+    "projetos",
+    "tecnologias",
+    "contato",
+  ];
+
   return (
     <nav className="flex w-full items-center justify-between px-15 pt-7">
       {/* COUNTRY FLAGS */}
@@ -12,16 +22,23 @@ function Navbar() {
       </div>
 
       {/* MENUBAR */}
-      <ul className="flex gap-5">
-        <li>sobre mim</li>
-        <li>projetos</li>
-        <li>tecnologias</li>
-        <li>contato</li>
+      <ul className="relative h-[50px] px-5 border-2 border-primary-purple rounded-full flex items-center justify-center gap-5">
+        {navItems.map((item, index) => (
+          <li
+            key={index}
+            onClick={() => setActiveItem(index)}
+            className={`transition-all duration-200 ${
+              activeItem === index ? "activeNavItem" : ""
+            }`}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
 
       {/* DARK MODE BUTTON */}
       <button>
-          <MoonIcon />
+        <MoonIcon />
       </button>
     </nav>
   );
